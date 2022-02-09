089077cad4ee2c5e8510188bed3bd64c68af8420
Yotty0404 <tyuke9216@hotmail.co.jp>
2022-02-09 21:55:51 +0900

    いったん完成

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ socketio = SocketIO(app)
 
 #ユーザー数
 user_count = 0
-#現在のテキスト
-text = ""
 
 p1_id = ""
 p2_id = ""
+p1_poke_name = ""
+p2_poke_name = ""
 
-is_p1_turn = True
-is_p1_ready = False
-is_p2_ready = False
+is_in_game = False
+
+#正解しているかどうか
+correct = [0, 0]
 
 @app.route('/')
 def index():
@@ -24,17 +25,19 @@ def index():
 #ユーザーが新しく接続すると実行
 @socketio.on('connect')
 def connect(auth):
-    global user_count, text, p1_id, p2_id, is_p1_ready, is_p2_ready
+    global user_count, p1_id, p2_id, p1_poke_name, p2_poke_name, is_in_game
     user_count += 1
 
     if user_count == 1:
-        is_p1_ready = False
-        is_p2_ready = False
+        p1_poke_name = ""
+        p2_poke_name = ""
         p1_id = request.sid
+        is_in_game = False
 
     elif user_count == 2:
-        is_p2_ready = False
+        p2_poke_name = ""
         p2_id = request.sid
+        is_in_game = False
 
     #接続者数の更新（全員向け）
     emit('count_update', {'user_count': user_count}, broadcast=True)
@@ -44,39 +47,88 @@ def connect(auth):
 #ユーザーの接続が切断すると実行
 @socketio.on('disconnect')
 def disconnect():
-    global user_count, is_p1_ready, is_p2_ready
+    global user_count, p1_poke_name, p2_poke_name, is_in_game, p1_id, p2_id
     user_count -= 1
 
     if user_count < 2:
-        is_p2_ready = False
+        p2_poke_name = ""
+        is_in_game = False
+        p1_id = p2_id
+        p2_id = ""
 
     if user_count < 1:
-        is_p1_ready = False
-        is_p2_ready = False
+        p1_poke_name = ""
+        p2_poke_name = ""
+        is_in_game = False
 
     #接続者数の更新（全員向け）
     emit('count_update', {'user_count': user_count}, broadcast=True)
 
 #ENTERボタンクリックで実行
 @socketio.on('btn_click')
-def text_update_request(json):
-    global is_p1_turn, is_p1_ready, is_p2_ready
+def btn_click(json):
+    global p1_poke_name, p2_poke_name, is_in_game, correct
     is_p1 = json["is_p1"]
+    poke_name = json["poke_name"]
     
-    if is_p1:
-        is_p1_ready = True
-        emit('update_answer', {'is_p1': is_p1 }, broadcast=True, include_self=False)
+    if not is_in_game:
+        if is_p1:
+            p1_poke_name = poke_name
+            emit('update_answer', {'is_p1': is_p1 }, broadcast=True, include_self=False)
+        else:
+            p2_poke_name = poke_name
+            emit('update_answer', {'is_p1': is_p1 }, broadcast=True, include_self=False)
+
+        if p1_poke_name != "" and p2_poke_name != "":
+            is_in_game = True
+            correct = [0, 0]
+            emit('battle_start', broadcast=True)
+
     else:
-        is_p2_ready = True
-        emit('update_answer', {'is_p1': is_p1 }, broadcast=True, include_self=False)
+        judges = []
+        if is_p1:
+            for i in range(5):
+                if p2_poke_name[i] == poke_name[i]:
+                    judges.append(1)
+                elif poke_name[i] in p2_poke_name:
+                    judges.append(2)
+                else:
+                    judges.append(0)
 
-    if is_p1_ready and is_p2_ready:
-        emit('battle_start', broadcast=True)
+            emit('judge', {'is_p1': is_p1,'poke_name':poke_name, 'judges':judges }, broadcast=True)
+
+            if max(judges) == min(judges) == 1:
+                correct[0] = 1
+        else:
+            for i in range(5):
+                if p1_poke_name[i] == poke_name[i]:
+                    judges.append(1)
+                elif poke_name[i] in p1_poke_name:
+                    judges.append(2)
+                else:
+                    judges.append(0)
+
+            if max(judges) == min(judges) == 1:
+                correct[1] = 1
+
+            emit('judge', {'is_p1': is_p1,'poke_name':poke_name, 'judges':judges }, broadcast=True)
+
+            if max(correct) == 1:
+                emit('end', {'correct' : correct}, broadcast=True)
+
+
+#ENDボタンクリックで実行
+@socketio.on('btn_end_click')
+def btn_end_click(json):
+    print(1)
+    global p1_poke_name, p2_poke_name, is_in_game, correct
+    correct = [0, 0]
+    p1_poke_name = ""
+    p2_poke_name = ""
+    is_in_game = False
+    emit('reset', broadcast=True)
 
 
 
-    #is_p1_turn = not bool(is_p1_turn)
-    #emit('change_turn', {'is_p1_turn': is_p1_turn }, broadcast=True,
-    #include_self=False)
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,30 @@
-﻿var socket = io();
+﻿var poke_data = [];
+
+//json取得
+$.getJSON("./static/json/pokedex.json", function (data) {
+    poke_data = data;
+});
+
+
+var socket = io();
 
 var p1_id = "";
 var p2_id = "";
 var is_in_game = false;
+var is_end = false;
 
 //接続者数の更新
-socket.on('count_update', function (msg) {
-    $('#user_count').html(msg.user_count);
+socket.on('count_update', function (data) {
+    $('#user_count').html(data.user_count);
 });
 
 //id更新
-socket.on('info_update', function (msg) {
-    $('#id1').html(msg.p1_id);
-    $('#id2').html(msg.p2_id);
+socket.on('info_update', function (data) {
+    /*    $('#id1').html(data.p1_id);*/
+    /*    $('#id2').html(data.p2_id);*/
 
-    p1_id = msg.p1_id;
-    p2_id = msg.p2_id;
+    p1_id = data.p1_id;
+    p2_id = data.p2_id;
 
 
     //プレイヤー判断
@@ -28,15 +37,15 @@ socket.on('info_update', function (msg) {
 });
 
 //正解を更新
-socket.on('update_answer', function (msg) {
-    $("#txtPokeName").prop("disabled", false);
+socket.on('update_answer', function (data) {
+    $("#txt_poke_name").prop("disabled", false);
     $("#btn").prop("disabled", false);
 
-    if (msg.is_p1) {
-        updateRow('?????', $("#answer_l"));
+    if (data.is_p1) {
+        update_row('?????', $("#answer_l"));
     }
     else {
-        updateRow('?????', $("#answer_r"));
+        update_row('?????', $("#answer_r"));
     }
 });
 
@@ -44,60 +53,201 @@ socket.on('update_answer', function (msg) {
 
 //バトルスタート
 socket.on('battle_start', async function () {
+    is_in_game = true;
+    is_end = false;
+    $('#turn').html('Player1のターン');
+
     //プレイヤー判断
     if (p1_id == socket.id) {
-        $("#txtPokeName").prop("disabled", false);
+        $("#txt_poke_name").prop("disabled", false);
         $("#btn").prop("disabled", false);
     }
     else if (p2_id == socket.id) {
-        $("#txtPokeName").prop("disabled", true);
+        $("#txt_poke_name").prop("disabled", true);
         $("#btn").prop("disabled", true);
     }
 
-    $(battle_start).removeClass("transparent");
+    $("#battle_start").html("BATTLE START");
+    $("#battle_start_container").removeClass("collapse");
+    $("#battle_start").removeClass("transparent");
     await sleep(2000);
-    $(battle_start).addClass("transparent");
+    $("#battle_start_container").addClass("collapse");
+    $("#battle_start").addClass("transparent");
 });
 
-//ターン変更
-socket.on('change_turn', function (msg) {
-    if (msg.is_p1) {
-        $('#turn').html('Player1のターン');
+//判定を反映
+socket.on('judge', function (data) {
+    if (data.is_p1) {
+        var row = $('<div class="row">');
+        $('#predict_r_container').append(row);
+        for (var i = 0; i < 5; i++) {
+            row.append('<div class="tile"></div>');
+        }
+
+        update_row_by_judge(data.poke_name, row, data.judges);
+        $('#predict_r_container').scrollTop($('#predict_r_container').get(0).scrollHeight);
+        $('#turn').html('Player2のターン');
+
+        //プレイヤー判断
+        if (p2_id == socket.id) {
+            $("#txt_poke_name").prop("disabled", false);
+            $("#btn").prop("disabled", false);
+        }
     }
     else {
-        $('#turn').html('Player2のターン');
-    }
-});
+        var row = $('<div class="row">');
+        $('#predict_l_container').append(row);
+        for (var i = 0; i < 5; i++) {
+            row.append('<div class="tile"></div>');
+        }
 
+        update_row_by_judge(data.poke_name, row, data.judges);
+        $('#predict_l_container').scrollTop($('#predict_l_container').get(0).scrollHeight);
+        $('#turn').html('Player1のターン');
 
-
-
-//ボタンクリック
-$(document).on('click', '#btn', function () {
-    $("#txtPokeName").prop("disabled", true);
-    $("#btn").prop("disabled", true);
-
-    var pokeName = $("#txtPokeName").val();
-    $("#txtPokeName").val("");
-
-    if (!is_in_game) {
         //プレイヤー判断
         if (p1_id == socket.id) {
-            updateRow(pokeName, $("#answer_l"));
-            socket.emit('btn_click', { is_p1: true });
+            $("#txt_poke_name").prop("disabled", false);
+            $("#btn").prop("disabled", false);
         }
-        else if (p2_id == socket.id) {
-            updateRow(pokeName, $("#answer_r"));
-            socket.emit('btn_click', { is_p1: false });
-        }
-    }
-    else {
-
     }
 });
 
+//勝敗表示
+socket.on('end', async function (data) {
+    if (is_end) {
+        return;
+    }
+
+    await sleep(2000);
+    is_end = true;
+
+    var msg = "";
+    if (data.correct[0] == 1 && data.correct[1] == 1) {
+        msg = "DRAW";
+    }
+    else if (data.correct[0] == 1) {
+        //プレイヤー判断
+        if (p1_id == socket.id) {
+            msg = "WIN!";
+            $("#battle_start").addClass("win");
+        }
+        else if (p2_id == socket.id) {
+            msg = "LOSE";
+            $("#battle_start").addClass("lose");
+        }
+    }
+    else if (data.correct[1] == 1) {
+        //プレイヤー判断
+        if (p1_id == socket.id) {
+            msg = "LOSE";
+            $("#battle_start").addClass("lose");
+        }
+        else if (p2_id == socket.id) {
+            msg = "WIN!";
+            $("#battle_start").addClass("win");
+        }
+    }
+
+    $("#txt_poke_name").prop("disabled", false);
+    $("#btn").prop("disabled", false);
+
+    $("#battle_start").html(msg);
+    $("#battle_start_container").removeClass("collapse");
+    $("#battle_start").removeClass("transparent");
+    await sleep(2000);
+    $("#battle_start_container").addClass("collapse");
+    $("#battle_start").addClass("transparent");
+    await sleep(1000);
+    $("#battle_start").removeClass("win");
+    $("#battle_start").removeClass("lose");
+    $("#btn_end").prop("disabled", false);
+});
+
+//リセット処理
+socket.on('reset', async function (data) {
+    reset_row();
+    $("#predict_l_container").empty();
+    $("#predict_r_container").empty();
+    $('#turn').html('');
+    $("#btn_end").prop("disabled", true);
+    is_end = false;
+    is_in_game = false;
+});
+
+
+
+
+
+//ENTERボタンクリック
+$(document).on('click', '#btn', function () {
+    var poke_name = $("#txt_poke_name").val();
+
+    check_poke_name(poke_name).then(result => {
+        if (!result) {
+            return;
+        }
+
+        if (!is_end) {
+            $("#txt_poke_name").prop("disabled", true);
+            $("#btn").prop("disabled", true);
+        }
+
+        $("#txt_poke_name").val("");
+
+        if (!is_in_game) {
+            //プレイヤー判断
+            if (p1_id == socket.id) {
+                update_row(poke_name, $("#answer_l"));
+                socket.emit('btn_click', { is_p1: true, poke_name: poke_name });
+            }
+            else if (p2_id == socket.id) {
+                update_row(poke_name, $("#answer_r"));
+                socket.emit('btn_click', { is_p1: false, poke_name: poke_name });
+            }
+        }
+        else {
+            //プレイヤー判断
+            if (p1_id == socket.id) {
+                socket.emit('btn_click', { is_p1: true, poke_name: poke_name });
+            }
+            else if (p2_id == socket.id) {
+                socket.emit('btn_click', { is_p1: false, poke_name: poke_name });
+            }
+        }
+    });
+});
+
+async function check_poke_name(poke_name) {
+    var data = "";
+    var rtn = true;
+
+    if (poke_name.length != 5) {
+        data = "5文字で入力してください";
+        rtn = false;
+    }
+
+    var index = poke_data.findIndex(x => x.name.japanese === poke_name);
+
+    if (rtn && index == -1) {
+        data = "そのポケモンはいません";
+        rtn = false;
+    }
+
+    if (!rtn) {
+        $("#msg").html(data);
+        $("#msg_container").removeClass("collapse");
+        $("#msg").removeClass("transparent");
+        await sleep(1500);
+        $("#msg_container").addClass("collapse");
+        $("#msg").addClass("transparent");
+    }
+
+    return rtn;
+}
+
 //テキストボックスでEnterキー押下時、ボタンクリックを発火
-$(document).on('keydown', '#txtPokeName', function (event) {
+$(document).on('keydown', '#txt_poke_name', function (event) {
     if (event.key === 'Enter') {
         $("#btn").click();
     }
@@ -108,16 +258,54 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function updateRow(pokeName, parent) {
-    for (var i = 0; i < pokeName.length; ++i) {
-        updateTile($(parent.children()[i]), pokeName.charAt(i))
+async function update_row(poke_name, parent) {
+    for (var i = 0; i < poke_name.length; ++i) {
+        update_tile($(parent.children()[i]), poke_name.charAt(i))
         await sleep(250);
     }
 }
 
-async function updateTile(tile, s) {
+async function update_tile(tile, s) {
     tile.addClass("tile_disappear");
     await sleep(250);
     tile.html(s);
+    tile.removeClass("tile_disappear");
+}
+
+async function update_row_by_judge(poke_name, parent, judges) {
+    await sleep(100);
+    for (var i = 0; i < poke_name.length; ++i) {
+        update_tile_by_judge($(parent.children()[i]), poke_name.charAt(i), judges[i])
+        await sleep(250);
+    }
+}
+
+async function update_tile_by_judge(tile, s, judge) {
+    tile.addClass("tile_disappear");
+    await sleep(250);
+    tile.html(s);
+    var class_name = `judge${judge}`;
+    tile.addClass(`judge${judge}`);
+    tile.removeClass("tile_disappear");
+}
+
+//ENDボタンクリック
+$(document).on('click', '#btn_end', function () {
+    /*    socket.emit('btn_end_click');*/
+    socket.emit('btn_end_click', { });
+});
+
+async function reset_row() {
+    for (var i = 0; i < 5; ++i) {
+        reset_tile($($("#answer_l").children()[i]))
+        reset_tile($($("#answer_r").children()[i]))
+        await sleep(250);
+    }
+}
+
+async function reset_tile(tile) {
+    tile.addClass("tile_disappear");
+    await sleep(250);
+    tile.html("");
     tile.removeClass("tile_disappear");
 }
