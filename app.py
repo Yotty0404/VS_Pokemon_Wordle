19f78b8c56e69edd85fc9dc480c94b939a48235a
Yotty0404 <tyuke9216@hotmail.co.jp>
2022-02-09 11:49:01 +0900

    入力された文字をタイルに更新するところまで実装

--- app.py
+++ app.py
@@ -5,47 +5,67 @@ import os
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-# ユーザー数
+#ユーザー数
 user_count = 0
-# 現在のテキスト
+#現在のテキスト
 text = ""
+
+p1_id = ""
+p2_id = ""
+
+is_p1_turn = True
+is_p1_ready = False
+is_p2_ready = False
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# ユーザーが新しく接続すると実行
+#ユーザーが新しく接続すると実行
 @socketio.on('connect')
 def connect(auth):
-    global user_count, text
+    global user_count, text, p1_id, p2_id
     user_count += 1
-    # 接続者数の更新（全員向け）
+
+    if user_count == 1:
+        p1_id = request.sid
+
+    elif user_count == 2:
+        p2_id = request.sid
+
+    #接続者数の更新（全員向け）
     emit('count_update', {'user_count': user_count}, broadcast=True)
-    # テキストエリアの更新
+    #テキストエリアの更新
     emit('text_update', {'text': text})
+    emit('info_update', {'p1_id': p1_id, 'p2_id': p2_id}, broadcast=True)
 
 
-# ユーザーの接続が切断すると実行
+#ユーザーの接続が切断すると実行
 @socketio.on('disconnect')
 def disconnect():
     global user_count
     user_count -= 1
-    # 接続者数の更新（全員向け）
+
+    #接続者数の更新（全員向け）
     emit('count_update', {'user_count': user_count}, broadcast=True)
 
 
-# テキストエリアが変更されたときに実行
+#テキストエリアが変更されたときに実行
 @socketio.on('text_update_request')
 def text_update_request(json):
     global text
     text = json["text"]
-    # 変更をリクエストした人以外に向けて送信する
-    # 全員向けに送信すると入力の途中でテキストエリアが変更されて日本語入力がうまくできない
+    #変更をリクエストした人以外に向けて送信する
+    #全員向けに送信すると入力の途中でテキストエリアが変更されて日本語入力がうまくできない
     emit('text_update', {'text': text}, broadcast=True, include_self=False)
 
 
+#ENTERボタンクリックで実行
+@socketio.on('btn_click')
+def text_update_request():
+    global is_p1_turn
+    is_p1_turn = not bool(is_p1_turn)
+    emit('change_turn', {'is_p1_turn': is_p1_turn }, broadcast=True, include_self=False)
+
 if __name__ == '__main__':
-    # 本番環境ではeventletやgeventを使うらしいが簡単のためデフォルトの開発用サーバーを使う
     socketio.run(app, debug=True)
-    #socketio.run(app)
-    #socketio.run(app, debug=True, host='0.0.0.0', port=5004)
--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,85 @@
 ﻿var socket = io();
 
-// 接続者数の更新
+var p1_id = "";
+var p2_id = "";
+var is_in_game = false;
+
+//接続者数の更新
 socket.on('count_update', function (msg) {
     $('#user_count').html(msg.user_count);
 });
 
-// テキストエリアの更新
+//テキストエリアの更新
 socket.on('text_update', function (msg) {
     $('#text').val(msg.text);
 });
 
-// テキストエリアが変更されると呼び出される
-$('#text').on('change keyup input', function () {
+//id更新
+socket.on('info_update', function (msg) {
+    $('#id1').html(msg.p1_id);
+    $('#id2').html(msg.p2_id);
+
+    p1_id = msg.p1_id;
+    p2_id = msg.p2_id;
 });
 
+//ターン変更
+socket.on('change_turn', function (msg) {
+    $("#btn").prop("disabled", false);
+
+    if (msg.is_p1_turn) {
+        $('#turn').html('Player1のターン');
+    }
+    else {
+        $('#turn').html('Player2のターン');
+    }
+});
 
 
 $(document).on('change keyup input', '#text', function () {
     socket.emit('text_update_request', { text: $(this).val() });
 });
+
+//ボタンクリック
+$(document).on('click', '#btn', function () {
+    $("#btn").prop("disabled", true);
+    socket.emit('btn_click');
+
+    var pokeName = $("#txtPokeName").val();
+    $("#txtPokeName").val("");
+
+    if (!is_in_game) {
+        if (p1_id == socket.id) {
+            updateRow(pokeName, $("#answer_l"));
+        }
+        else if (p2_id == socket.id) {
+            updateRow(pokeName, $("#answer_r"));
+        }
+    }
+});
+
+//テキストボックスでEnterキー押下時、ボタンクリックを発火
+$(document).on('keydown', '#txtPokeName', function (event) {
+    if (event.key === 'Enter') {
+        $("#btn").click();
+    }
+});
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function updateRow(pokeName, parent) {
+    for (var i = 0; i < pokeName.length; ++i) {
+        updateTile($(parent.children()[i]), pokeName.charAt(i))
+        await sleep(250);
+    }
+}
+
+async function updateTile(tile, s) {
+    tile.addClass("tile_disappear");
+    await sleep(250);
+    tile.html(s);
+    tile.removeClass("tile_disappear");
+}
